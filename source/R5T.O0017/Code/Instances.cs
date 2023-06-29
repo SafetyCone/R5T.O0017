b412c47e5ccfac41276597de195fb48ea5c44ad1
davidcoats <david@getsafetycone.com>
2023-06-29 01:24:17 -0700

    Interim changes.

--- source/R5T.O0017/Code/Instances.cs
+++ source/R5T.O0017/Code/Instances.cs
@@ -7,5 +7,8 @@ namespace R5T.O0017
     {
         public static L0036.IGitHubRepositoryContextOperator GitHubRepositoryContextOperator => L0036.GitHubRepositoryContextOperator.Instance;
         public static L0036.IGitHubRepositoryContextOperations GitHubRepositoryContextOperations => L0036.GitHubRepositoryContextOperations.Instance;
+        public static F0019.IGitOperator GitOperator => F0019.GitOperator.Instance;
+        public static O0023.IGitOperations GitOperations => O0023.GitOperations.Instance;
+        public static F0041.IGitHubOperator GitHubOperator => F0041.GitHubOperator.Instance;
     }
 }
--- source/R5T.O0017/Code/Values/IGitHubRepositoryOperations.cs
+++ source/R5T.O0017/Code/Values/IGitHubRepositoryOperations.cs
@@ -3,14 +3,40 @@ using System.Threading.Tasks;
 
 using R5T.T0131;
 using R5T.T0159;
+using R5T.T0180;
 using R5T.T0184;
 using R5T.T0186;
+using R5T.T0198;
+using R5T.T0198.Extensions;
+using R5T.T0200;
+
 
 namespace R5T.O0017
 {
     [ValuesMarker]
     public partial interface IGitHubRepositoryOperations : IValuesMarker
     {
+        public Task<bool> Is_Private(IPath withinRepositoryPath)
+        {
+            var repositoryDirectoryPath = Instances.GitOperations.Get_RepositoryDirectoryPath(withinRepositoryPath);
+
+            var output = this.Is_Private(repositoryDirectoryPath);
+            return output;
+        }
+
+        public Task<bool> Is_Private(IRepositoryDirectoryPath repositoryDirectoryPath)
+        {
+            var repositoryUrl = Instances.GitOperations.Get_RepositoryUrl(repositoryDirectoryPath);
+
+            return this.Is_Private(repositoryUrl);
+        }
+
+        public Task<bool> Is_Private(IRepositoryUrl repositoryUrl)
+        {
+            var output = Instances.GitHubOperator.Is_Private(repositoryUrl.Value);
+            return output;
+        }
+
         public async Task Delete_Repository(
             IGitHubRepositoryName repositoryName,
             IGitHubRepositoryOwnerName ownerName,
